3d56afab622f2a87967df5273fc51dbab2d323e3
Kevin Sun <kevinsun8803@gmail.com>
2020-12-28 23:55:44 -0500

    init commit

---
+++ app/src/main/java/com/dev/door_dash/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dev.door_dash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
